f9a308890e6b6615738e9f7c25ea13e2cde01cc4
ARYAN-01-GARG <aryangarg30842@gmail.com>
2024-10-16 01:02:25 +0530

    Healthy Server Created to add Docker file.

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const app = express();
+const PORT = 3000 || process.env.PORT;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.join(__dirname, 'src' ,'public')));
+
+app.get('/', (req , res ) => {
+    res.sendFile(path.join(__dirname, 'src','public','html', 'index.html'));
+})
+
+app.listen(PORT , () => {
+    console.log(`Server is running on port ${PORT}`);
+})
